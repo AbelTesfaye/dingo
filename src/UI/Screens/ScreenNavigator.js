c64b0ec3dd8bf6a5db132583a6df5cb6acceb2cc
Abel Tesfaye <abeltesfaye45@gmail.com>
2019-04-28 15:29:11 +0300

    ui: make recursive layout generator using JSON object

--- src/UI/Screens/ScreenNavigator.js
+++ src/UI/Screens/ScreenNavigator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View,Switch,Text } from "react-native";
 import Icon from "react-native-ionicons";
 import { TabBar, TabView } from "react-native-tab-view";
 import { MiniPlayer } from "../CustomModules/JS/MiniPlayer";
@@ -7,8 +7,99 @@ import { PageHome } from "../Pages/PageHome";
 import { PageLibrary } from "../Pages/PageLibrary";
 import { PageSearch } from "../Pages/PageSearch";
 import Animated from "react-native-reanimated";
+import { TextInput } from "react-native-gesture-handler";
 
 const { width, height } = Dimensions.get("window");
+getObject = (obj, k_v) => {
+	var result = null;
+	if (obj instanceof Array) {
+		for (var i = 0; i < obj.length; i++) {
+			result = getObject(obj[i], k_v);
+			if (result) {
+				break;
+			}
+		}
+	} else {
+		for (var prop in obj) {
+			if (prop == 'setting_key' && k_v == obj['setting_key']) {
+				return obj;
+			}
+			if (obj[prop] instanceof Object || obj[prop] instanceof Array) {
+				result = getObject(obj[prop], k_v);
+				if (result) {
+					break;
+				}
+			}
+		}
+	}
+	return result;
+};
+
+const saveSetting = (key, value) => {
+	return (getObject(json, key).currentValue = value);
+};
+const getSetting = key => {
+	return getObject(json, key);
+};
+
+const flattenMenu = settingsArr => {
+	return settingsArr.map(i => (
+		<View
+			style={{
+				padding: 10,
+			}}
+		>
+			{(i.type === 'boolean' && (
+				<View style={{ flexDirection: 'row' }}>
+					<Text>{i.title}</Text>
+					<Switch value={i.currentValue} />
+				</View>
+			)) ||
+				(i.type === 'number' && (
+					<View>
+						<Text>{i.title}</Text>
+						<TextInput
+							placeholder={i.currentValue.toString()}
+							keyboardType="decimal-pad"
+							style={{
+								backgroundColor: 'yellow',
+							}}
+						/>
+					</View>
+				)) ||
+				(i.type === 'text' && (
+					<View>
+						<Text>{i.title}</Text>
+						<TextInput
+							placeholder={i.currentValue}
+							keyboardType="default"
+							style={{
+								backgroundColor: 'orange',
+							}}
+						/>
+					</View>
+				)) ||
+				(i.type === 'submenu' && (
+					<View style={{ backgroundColor: 'green', paddingLeft: 10 }}>
+						<Text>{i.title}</Text>
+						<Text>{i.description}</Text>
+						{flattenMenu(i.contents)}
+					</View>
+				)) ||
+				(i.type === 'menu' && (
+					<View style={{ backgroundColor: 'blue', paddingLeft: 10 }}>
+						<Text>{i.title}</Text>
+						<Text>{i.description}</Text>
+						{flattenMenu(i.contents)}
+					</View>
+				)) || <Text style={{ backgroundColor: 'red' }}> Item type not found: {i.type}</Text>}
+		</View>
+	));
+};
+
+const renderSettings = settingsObj => {
+	return flattenMenu(settingsObj.settings);
+};
 
 export class ScreenNavigator extends React.Component {
   constructor(props) {
@@ -129,7 +220,7 @@ export class ScreenNavigator extends React.Component {
                 case "PAGE_LIBRARY":
                   return <PageLibrary AppInstance={this.AppInstance} />;
                 case "PAGE_SETTINGS":
-                  return <View/>;
+                  return <View>{renderSettings(settingsObj)}</View>;
                 default:
                   return null;
               }

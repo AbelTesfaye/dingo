9c4a03b8787cbcbc8ab071a5de6be6ff388497da
Abel Tesfaye <abeltesfaye45@gmail.com>
2019-05-05 13:50:13 +0300

    ui: faster navigation drawer transitions

--- src/UI/Screens/ScreenNavigator.js
+++ src/UI/Screens/ScreenNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View, TouchableWithoutFeedback } from 'react-native';
 import Icon from 'react-native-ionicons';
-import { TabBar, TabView } from 'react-native-tab-view';
+import { TabView } from 'react-native-tab-view';
 import { MiniPlayer } from '../CustomModules/JS/MiniPlayer';
 import { PageHome } from '../Pages/PageHome';
 import { PageLibrary } from '../Pages/PageLibrary';
@@ -17,28 +17,52 @@ export class ScreenNavigator extends React.Component {
 		this.state = {
 			index: 0,
 			routes: [
-				{ key: 'PAGE_HOME', icon: 'home', color: [255, 132, 0] },
-				{ key: 'PAGE_SEARCH', icon: 'search', color: [255, 132, 0] },
-				{ key: 'PAGE_LIBRARY', icon: 'ios-albums', color: [255, 132, 0] },
-				{ key: 'PAGE_SETTINGS', icon: 'settings', color: [255, 132, 0] },
+				{ key: 'PAGE_HOME', icon: 'home', color: '#ff8400' },
+				{ key: 'PAGE_SEARCH', icon: 'search', color: '#ff8400' },
+				{ key: 'PAGE_LIBRARY', icon: 'ios-albums', color: '#ff8400' },
+				{ key: 'PAGE_SETTINGS', icon: 'settings', color: '#ff8400' },
 			],
 		};
 		this.AppInstance = this.props.AppInstance;
+		this.disableAnimations = true;
 	}
-	_renderTabBar = props => (
-		<View>
-			<MiniPlayer style={{}} AppInstance={this.AppInstance} />
-			<TabBar
-				{...props}
-				renderIcon={this._renderIcon}
-				renderIndicator={this._renderIndicator}
-				style={styles.tabbar}
-				useNativeDriver={true}
-			/>
-		</View>
-	);
-	_renderIndicator = props => {
+
+	_renderTabBar = props => {
+		return (
+			<View
+				style={{
+					justifyContent: 'center',
+				}}
+			>
+				<MiniPlayer style={{}} AppInstance={this.AppInstance} />
+				<View style={{ ...styles.tabbar }}>
+					{props.navigationState.routes.map((route, index) => {
+						return (
+							<TouchableWithoutFeedback
+								key={route.key}
+								onPress={() => {
+									this.disableAnimations = true;
+									props.jumpTo(route.key);
+									this.forceUpdate();
+								}}
+							>
+								<View style={{ flex: 1 }}>
+									{this._renderItem({ ...props, disableAnimations: this.disableAnimations })({
+										route,
+										index,
+									})}
+								</View>
+							</TouchableWithoutFeedback>
+						);
+					})}
+				</View>
+			</View>
+		);
+	};
+
+	_renderItem = props => ({ route, index }) => {
 		const { width, position, navigationState } = props;
+
 		const inputRange = [0, 0.48, 0.49, 0.51, 0.52, 1, 1.48, 1.49, 1.51, 1.52, 2, 2.48, 2.49, 2.51, 2.52, 3];
 
 		const scale = Animated.interpolate(position, {
@@ -50,32 +74,41 @@ export class ScreenNavigator extends React.Component {
 			inputRange,
 			outputRange: inputRange.map(x => {
 				const d = (x - Math.trunc(x)).toFixed(2);
-				return d === (0.49).toFixed(2) || d === (0.51).toFixed(2) ? 0 : 1;
+				return d === (0.49).toFixed(2) || d === (0.51).toFixed(2) || Math.round(x) !== index ? 0 : 1;
 			}),
 		});
-
-		const translateX = Animated.interpolate(position, {
-			inputRange: inputRange,
-			outputRange: inputRange.map(x => Math.round(x) * width),
-		});
-
-		const backgroundColor = Animated.interpolate(position, {
-			inputRange,
-			outputRange: inputRange.map(x => Animated.color(...navigationState.routes[Math.round(x)].color)),
-		});
-
+		disableAnimations = props.disableAnimations;
 		return (
-			<Animated.View
-				style={[
-					styles.container,
+			<View style={{ ...styles.tab, alignItems: 'center', justifyContent: 'center' }}>
+				<Animated.View style={[styles.item]}>
 					{
-						width: `${100 / navigationState.routes.length}%`,
-						transform: [{ translateX }],
-					},
-				]}
-			>
-				<Animated.View style={[styles.indicator, { opacity, backgroundColor, transform: [{ scale }] }]} />
-			</Animated.View>
+						<Animated.View
+							style={[
+								styles.indicator,
+								{
+									position: 'absolute',
+									opacity: disableAnimations
+										? props.navigationState.index === index
+											? 1
+											: 0
+										: opacity,
+									transform: [
+										{
+											scale: disableAnimations
+												? index === props.navigationState.index
+													? 2
+													: 0
+												: scale,
+										},
+									],
+									backgroundColor: route.color,
+								},
+							]}
+						/>
+					}
+					{this._renderIcon({ route })}
+				</Animated.View>
+			</View>
 		);
 	};
 
@@ -96,8 +129,15 @@ export class ScreenNavigator extends React.Component {
 				>
 					<TabView
 						navigationState={this.state}
-						renderTabBar={this._renderTabBar}
+						renderTabBar={p => this._renderTabBar({ ...p, width: width })}
 						tabBarPosition={'bottom'}
+						onSwipeStart={() => {
+							this.disableAnimations = false;
+							this.forceUpdate();
+						}}
+						onSwipeEnd={() => {
+							this.disableAnimations = true;
+						}}
 						renderScene={({ route }) => {
 							switch (route.key) {
 								case 'PAGE_HOME':
@@ -136,18 +176,50 @@ const styles = StyleSheet.create({
 		backgroundColor: '#FFF',
 	},
 
-	tabbar: {
-		backgroundColor: '#fff',
-		overflow: 'hidden',
-	},
-	icon: {
-		backgroundColor: 'transparent',
-		color: '#000',
-	},
 	indicator: {
 		width: 48,
 		height: 48,
 		borderRadius: 24,
 		margin: 6,
+	},
+	tabbar: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		backgroundColor: '#fafafa',
+		height: 48,
+		overflow: 'hidden',
+
+		elevation: 4,
+		shadowColor: 'black',
+		shadowOpacity: 0.1,
+		shadowRadius: StyleSheet.hairlineWidth,
+		shadowOffset: {
+			height: StyleSheet.hairlineWidth,
+		},
+		zIndex: 1,
+	},
+	tab: {
+		flex: 1,
+		alignItems: 'center',
+	},
+	item: {
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	activeItem: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
+	icon: {
+		color: '#000',
+	},
+	label: {
+		fontSize: 10,
+		marginTop: 3,
+		marginBottom: 1.5,
+		backgroundColor: 'transparent',
 	},
 });
